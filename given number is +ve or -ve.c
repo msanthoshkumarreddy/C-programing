6a1a870ecdc9b25049eb7779e35b0039e90126f5
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-11-25 21:55:10 +0530

    Add files via upload

---
+++ given number is +ve or -ve.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter a number");
+	scanf("%d",&n);
+	if(n>0)
+	printf("given number is positive");
+	else 
+	{
+	printf("given number is nagetive");
+	}
+	return 0;
+}
+	
