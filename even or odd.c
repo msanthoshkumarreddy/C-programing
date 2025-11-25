bfa7590848bc550df7dca57b9c2164990f883f8a
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-11-25 21:54:09 +0530

    Add files via upload

---
+++ even or odd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%2==0){
+    printf("Even number");
+    }
+    else
+    printf("Odd number");
+    }
