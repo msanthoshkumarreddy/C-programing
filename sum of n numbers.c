195f974fb1fd54d565b22906b68e67a9771f67a6
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-11-27 08:18:11 +0530

    sum of n numbers.c

--- sum of n numbers.c
+++ sum of n numbers.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+#include<stdio.h>
 void main(){
     int n=3,i,a,sum=0;
     for(i=1;i<=n;i++) 
     {
-    printf("Enter number");
+    printf("enter number");
     scanf("%d",&a);
     sum=sum+a;  
     }
-    printf("Sum of numbers is %d",sum);
+    printf("sum of numbers is %d",sum);
 }
