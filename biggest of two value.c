234cf43f3d95b89a43726747b2b6feec3a57a5fd
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-11-27 18:00:12 +0530

    Add files via upload

---
+++ biggest of two value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{int a,b;
+printf("enter a and b value");
+scanf("%d %d",&a,&b);
+if(a>b)
+printf("a value is biggest ",a);
+else
+printf("b value biggest is %d",b);
+}
