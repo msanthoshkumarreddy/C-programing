7620a2bab425a72b453da864c289e64aa66ccffa
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-11-28 08:54:53 +0530

    Add files via upload

---
+++ prime number or not.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{int n=6,i,c=0;
+for(i=1;i<=n;i++){
+    if(n%i==0){
+        c++;
+    }
+    }
+if(c==2){
+    printf("prime number");
+}
+    else
+    printf("not a prime number");
+
+return 0;
+}
