e6fec2815bd91b4a0696c31a3664762d7c87cddf
msanthoshkumarreddy <25kq1a54c1@pace.ac.in>
2025-12-15 16:19:00 +0530

    Add files via upload

---
+++ max and mim using array.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+int n,a[20],i,max,min;
+scanf("%d",&n);
+for(i=0;i<n;i++)
+scanf("%d",&a[i]);
+max=a[0];
+min=a[0];
+for(i=0;i<n;i++){
+if(a[i]>max){
+max=a[i];
+}
+if(a[i]<min){
+min=a[i];
+}
+}
+printf("maximum=%d\nminimum=%d",max,min);
+}
